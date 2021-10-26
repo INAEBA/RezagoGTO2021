1e9d943b2c7ecf0e24060018b1c0afa699c8e809
inaeba <mrochag@inaeba.edu.mx>
2021-10-26 15:24:14 -0500

    primera edicion

---
+++ data/CoordinacindeZona_6.js
@@ -0,0 +1,26 @@
+var json_CoordinacindeZona_6 = {
+"type": "FeatureCollection",
+"name": "CoordinacindeZona_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "ABASOLO" }, "geometry": { "type": "Point", "coordinates": [ -101.525256617380194, 20.448791265683113 ] } },
+{ "type": "Feature", "properties": { "Name": "ACAMBARO" }, "geometry": { "type": "Point", "coordinates": [ -100.721028203029704, 20.037387131399267 ] } },
+{ "type": "Feature", "properties": { "Name": "CELAYA" }, "geometry": { "type": "Point", "coordinates": [ -100.815544757463215, 20.523193961352472 ] } },
+{ "type": "Feature", "properties": { "Name": "DOLORES HIDALGO" }, "geometry": { "type": "Point", "coordinates": [ -100.932439243951194, 21.149384489289709 ] } },
+{ "type": "Feature", "properties": { "Name": "IRAPUATO" }, "geometry": { "type": "Point", "coordinates": [ -101.334962395456003, 20.716279272598346 ] } },
+{ "type": "Feature", "properties": { "Name": "LEON SUR Y CENTRO" }, "geometry": { "type": "Point", "coordinates": [ -101.688873467957904, 21.133029996194477 ] } },
+{ "type": "Feature", "properties": { "Name": "LEON NORTE" }, "geometry": { "type": "Point", "coordinates": [ -101.667666025937905, 21.120241421045478 ] } },
+{ "type": "Feature", "properties": { "Name": "PENJAMO" }, "geometry": { "type": "Point", "coordinates": [ -101.723428828328395, 20.423990942883389 ] } },
+{ "type": "Feature", "properties": { "Name": "PURISIMA DEL RINCON" }, "geometry": { "type": "Point", "coordinates": [ -101.875145321972397, 21.024166581250171 ] } },
+{ "type": "Feature", "properties": { "Name": "SALAMANCA" }, "geometry": { "type": "Point", "coordinates": [ -101.193713370654919, 20.580063571257771 ] } },
+{ "type": "Feature", "properties": { "Name": "SALVATIERRA" }, "geometry": { "type": "Point", "coordinates": [ -100.879964722594707, 20.212818167250287 ] } },
+{ "type": "Feature", "properties": { "Name": "SAN FELIPE" }, "geometry": { "type": "Point", "coordinates": [ -101.216800275364093, 21.478503658992679 ] } },
+{ "type": "Feature", "properties": { "Name": "SAN JOSE ITURBIDE" }, "geometry": { "type": "Point", "coordinates": [ -100.380932095246905, 20.999924596023106 ] } },
+{ "type": "Feature", "properties": { "Name": "SAN LUIS DE LA PAZ" }, "geometry": { "type": "Point", "coordinates": [ -100.509845581663797, 21.292207689011256 ] } },
+{ "type": "Feature", "properties": { "Name": "SAN MIGUEL DE ALLENDE" }, "geometry": { "type": "Point", "coordinates": [ -100.755597337858902, 20.901742818204937 ] } },
+{ "type": "Feature", "properties": { "Name": "SILAO" }, "geometry": { "type": "Point", "coordinates": [ -101.430993136524606, 20.94337185107106 ] } },
+{ "type": "Feature", "properties": { "Name": "VALLE DE SANTIAGO" }, "geometry": { "type": "Point", "coordinates": [ -101.179007353384605, 20.390656376549945 ] } },
+{ "type": "Feature", "properties": { "Name": "VILLAGRAN" }, "geometry": { "type": "Point", "coordinates": [ -100.991469479746698, 20.511608318588014 ] } },
+{ "type": "Feature", "properties": { "Name": "YURIRIA" }, "geometry": { "type": "Point", "coordinates": [ -101.116564456377503, 20.218116980829787 ] } }
+]
+}
